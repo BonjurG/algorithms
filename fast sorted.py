76cf39a621a48d13e97ce3f41f67c3ab36785874
BonjurG <hmgrem@yandex.ru>
2022-11-09 19:43:28 +0500

    algho fast sorted was added

--- fast sorted.py
+++ fast sorted.py
@@ -26,7 +26,19 @@ def max_number_rec(arr):
     return arr[0] if arr[0] > sub_max else sub_max
 
 
+def quicksort(array):
+    if len(array) < 2:
+        return array  # base
+    else:
+        pivot = array[0]  # recurs
+        less = [i for i in array[1:] if i < pivot]  # arr less then pivot
+        greater = [i for i in array[1:] if i > pivot]  # arr more then pivot
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+
 # print(summa([1, 2, 3]))
 # print(recurs_count([1, 2, 3]))
-#print(max_number([1, 2, 3, 4, 11]))
+# print(max_number([1, 2, 3, 4, 11]))
 print(max_number_rec([1, 2, 3, 4, 11]))
+# print(quicksort([]))
+print(quicksort([11, 3, 5, 2, 6]))
